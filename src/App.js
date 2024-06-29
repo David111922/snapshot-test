5c0ed0c38e88eaedc7dd69fce8760c0ed2262536
David111922 <salve62@yahoo.com>
2024-06-28 21:13:47 -0700

    snapshot of snapshot

--- src/App.js
+++ src/App.js
@@ -6,21 +6,7 @@ import GitHubCard from './GitHubCard/GitHubCard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p><h2> failsafe-falstaff</h2>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
       <GitHubCard/>
     </div>
   );
